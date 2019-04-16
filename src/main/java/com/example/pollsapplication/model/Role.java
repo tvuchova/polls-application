824e10a80208a7a242026ff57a49507ec9412fc5
tvuchova <temenuzhka.vuchova@tick42.com>
2019-04-16 16:28:09 +0300

    changes adding some annotations

--- src/main/java/com/example/pollsapplication/model/Role.java
+++ src/main/java/com/example/pollsapplication/model/Role.java
@@ -3,12 +3,16 @@ package com.example.pollsapplication.model;
 import org.hibernate.annotations.NaturalId;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "roles")
 public class Role
 {
     @Id
--- src/main/java/com/example/pollsapplication/repository/RoleRepository.java
+++ src/main/java/com/example/pollsapplication/repository/RoleRepository.java
@@ -3,9 +3,11 @@ package com.example.pollsapplication.repository;
 import com.example.pollsapplication.model.Role;
 import com.example.pollsapplication.model.RoleName;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface RoleRepository extends JpaRepository<Role,Long>
 {
     Optional<Role> findByName(RoleName roleName);
